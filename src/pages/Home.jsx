214390cc222ea5c13b8f58544d645c940f6dbe4d
Lokesh <lokeshalli1807@gmail.com>
2024-11-16 19:11:51 +0530

    test commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,6 +33,7 @@ function Home() {
     <div className="w-full py-8">
       <Container>
         <div className="flex flex-wrap">
+          <p>testing</p>
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
               <PostCard {...post} />
